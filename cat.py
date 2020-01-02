d7a811a479d975ec9b44d7ee3d7e0f0b7b8979ff
kazuhiko-itani <kazuhiko.itani@gmail.com>
2020-01-02 14:45:01 +0900

    add cat.py

---
+++ cat.py
@@ -0,0 +1,30 @@
+import sys
+
+def main():
+  args = sys.argv
+
+  if len(args) < 2:
+    print(f'{args[0]}: file name not given');
+    return
+
+  for i in range(len(args)):
+    if i == 0:
+      continue
+
+    do_cat(args[i])
+
+def do_cat(filename):
+  try:
+    data = open(filename, "r")
+
+    for line in data:
+      print(line, end="")
+  except:
+    print(f'{filename} is not file');
+  
+
+  data.close()
+
+if __name__ == "__main__":
+  main()
+  
